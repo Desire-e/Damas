6dd43390ce0eecda37bc0804c0372593da2dd6b3
Desire-e <desireeruedatorresalmeria@gmail.com>
2024-11-14 14:37:39 +0100

    Creados equals() y hashcode();

--- src/main/java/org/iesalandalus/programacion/damas/modelo/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/damas/modelo/Posicion.java
@@ -1,5 +1,7 @@
 package org.iesalandalus.programacion.damas.modelo;
 
+import java.util.Objects;
+
 public class Posicion {
 
     private int fila;
@@ -37,6 +39,30 @@ public class Posicion {
 
         } else
             throw new IllegalArgumentException("ERROR: La columna debe ser desde la 'a' hasta la 'h'");
+    }
+
+
+    //Métodos equals() y hashcode()
+    @Override
+    /* Verifica los enlaces (códigos hash) de dos objetos para determinar su equivalencia */
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        /* Si el objeto con el que me estoy comparando soy yo, devuelve 'true'.*/
+        if (o == null || getClass() != o.getClass()) return false;
+        /* Si 'o' no tiene valor, o la clase del objeto en cuestión (this) es != de la clase del objeto o,
+        devuelve false. (Es falso que sean el mismo objeto). */
+
+        Posicion other = (Posicion) o;
+        /* Convierte 'o' de tipo Object a tipo Posicion, y asigna su valor a objeto 'other' tipo Posicion */
+        return fila == other.fila && columna == other.columna;
+        /* Devuelve fila del objeto other, y columna del objeto other */
+    }
+
+    @Override
+    /* Mapea los datos en un conjunto de caracteres (crea un código para esos datos) */
+    public int hashCode() {
+        return Objects.hash(fila, columna);
+        /* Devuelve el hash de los objetos fila y columna */
     }
 
 
