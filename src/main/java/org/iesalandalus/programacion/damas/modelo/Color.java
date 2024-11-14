cfbe4f747beec8f610433131e7f6e17e7fc0e45b
Desire-e <desireeruedatorresalmeria@gmail.com>
2024-11-14 12:49:53 +0100

    Creado constructor Posicion(), añadido retoque en método toString() de los enumerados (cadenas de carácteres concatenadas a las variables cadenaAMostrar);

--- src/main/java/org/iesalandalus/programacion/damas/modelo/Color.java
+++ src/main/java/org/iesalandalus/programacion/damas/modelo/Color.java
@@ -11,6 +11,6 @@ public enum Color {
 
     @Override
     public String toString() {
-        return cadenaAMostrar;
+        return "Color: " + cadenaAMostrar;
     }
 }
--- src/main/java/org/iesalandalus/programacion/damas/modelo/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/damas/modelo/Posicion.java
@@ -5,6 +5,15 @@ public class Posicion {
     private int fila;
     private char columna;
 
+
+
+    public Posicion(int fila, char columna){
+        setFila(fila);
+        setColumna(columna);
+    }
+
+
+
     public int getFila() { return fila; }
     public void setFila(int fila){
         if (fila<1 || fila>8){
