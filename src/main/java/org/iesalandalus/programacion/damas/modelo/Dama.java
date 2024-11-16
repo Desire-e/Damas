d170f7f7aa732ec9b8d9032c9a1df92336db6326
Desire-e <desireeruedatorresalmeria@gmail.com>
2024-11-17 00:09:05 +0100

    Modificado método toString

--- src/main/java/org/iesalandalus/programacion/damas/modelo/Dama.java
+++ src/main/java/org/iesalandalus/programacion/damas/modelo/Dama.java
@@ -172,7 +172,6 @@ public class Dama {
         return "Dama{" +
                 "color=" + color +
                 ", posicion=" + posicion +
-                ", esDamaEspecial=" + esDamaEspecial +
                 '}';
     }
 
