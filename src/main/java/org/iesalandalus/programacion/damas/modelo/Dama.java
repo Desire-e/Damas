f0773b2af997cad5d27c978ce1469f8e02971807
Desire-e <desireeruedatorresalmeria@gmail.com>
2024-11-16 16:02:55 +0100

    Modificado método crearPosicionInicial;

--- src/main/java/org/iesalandalus/programacion/damas/modelo/Dama.java
+++ src/main/java/org/iesalandalus/programacion/damas/modelo/Dama.java
@@ -1,34 +1,11 @@
 package org.iesalandalus.programacion.damas.modelo;
 
-import java.util.Random;
-
 public class Dama {
     private Color color;
     private Posicion posicion;
-    private boolean esDamaEspecial = false;
+    private boolean esDamaEspecial;
 
 
-    //getters y setters del tipo Color
-    public Color getColor(){
-        return color;
-    }
-    private void setColor(Color color) {
-        if (color == null){
-            throw new NullPointerException("El color es nulo.");
-        }
-        this.color = color;
-    }
-
-    //getters y setters del tipo Posicion
-    public Posicion getPosicion(){
-        return posicion;
-    }
-    public void setPosicion(Posicion posicion){
-        if (posicion == null){
-            throw new NullPointerException("La posición es nula.");
-        }
-        this.posicion = posicion;
-    }
 
 
     //Constructor por defecto
@@ -36,20 +13,63 @@ public class Dama {
         setColor(Color.BLANCO);
         //Setter llama al método que crea posición aleatoria inicial, y se asigna como parámetro el resultado
         setPosicion(crearPosicionInicial());
+        this.esDamaEspecial = false;
+    }
+
+    //Constructor copia
+        // Según lo que reciba este constructor, será    Color color = Color.BLANCO o Color.NEGRO;
+        //Quizás habrá que crear un menú para elegir color, donde este pase el parámetro que significará
+        // un color u otro, que será pasado a este constructor.
+    public Dama (Color color){
+        setColor(color);
+        setPosicion(crearPosicionInicial());
+        this.esDamaEspecial = false;
     }
 
 
+
+
+    //getter y setter de variable tipo Color
+    public Color getColor(){
+        return color;
+    }
+    private void setColor(Color color) {
+        if (color == null){
+            throw new IllegalArgumentException("El color es nulo.");
+        }
+        this.color = color;
+    }
+
+    //getter y setter de variable tipo Posicion
+    public Posicion getPosicion(){
+        return posicion; /* return new Posicion(posicion); */
+    }
+    public void setPosicion(Posicion posicion){
+        if (posicion == null){
+            throw new IllegalArgumentException("La posición es nula.");
+        }
+        this.posicion = posicion; /* this.posicion = new Posicion(posicion); */
+    }
+
+
+
+
     // Método para generar posición inicial
+
     private Posicion crearPosicionInicial() {
+        // Definir las filas iniciales dependiendo del color de la dama (de 1 a 8)
         int filaInicial;
+        if (color == Color.BLANCO) {
+            filaInicial = (int)(Math.random()*3)+1;  // Fila de 1 a 3
+        } else {
+            filaInicial = (int)(Math.random()*3)+6;  // Fila de 6 a 8
+        }
+
+        // Generar una columna dependiendo de la fila (de 'a' a 'h')
         char columnaInicial = 'a';
 
-        //Fila aleatoria entre 1 y 3
-        filaInicial = (int) (Math.random() * 3) + 1;
-
-        //Columna dependiendo de la fila
-        if (filaInicial == 1 || filaInicial == 3) {
-            //Enumeración de opción aleatoria entre 0 y 3 (cada valor de opción representa una columna)
+        if (filaInicial == 1 || filaInicial == 3 || filaInicial == 7) {
+            //Enumeración de opción aleatoria, entre 0 y 3 (cada valor de opción representa una columna)
             int opcion = (int) (Math.random() * 4);
             switch (opcion) {
                 case 0 -> columnaInicial = 'a';
@@ -57,7 +77,7 @@ public class Dama {
                 case 2 -> columnaInicial = 'e';
                 case 3 -> columnaInicial = 'g';
             }
-        } else if (filaInicial == 2) {
+        } else if (filaInicial == 2 || filaInicial == 8 || filaInicial == 6) {
             int opcion = (int) (Math.random() * 4);
             switch (opcion) {
                 case 0 -> columnaInicial = 'b';
@@ -66,55 +86,54 @@ public class Dama {
                 case 3 -> columnaInicial = 'h';
             }
         }
+
         Posicion posicionInicial = new Posicion(filaInicial, columnaInicial);
         return posicionInicial;
+        /*  return new Posicion(filaInicial, columnaInicial);  */
     }
 
+
+
+
+    /* Crea el método mover que acepte como parámetros una Direccion y el número de pasos que se moverá.
+    Deberás tener en cuenta las siguientes restricciones: Realiza un commit.
+        * La dirección no puede ser nula o de lo contrario debe lanzar una excepción adecuada
+          (NullPointerException o IllegalArgumentException) con el mensaje adecuado.
+            * Si la dama todavía no se ha convertido en dama especial, solamente podrá moverse en un dirección
+              que le permita avanzar en el tablero y nunca retroceder. Noreste o Noroeste (si es una dama blanca)
+              y Sureste o Suroeste (si es una dama negra).
+        * El número de casillas que se mueve la dama deberá ser un número entero positivo, >=que 1.
+            * Si la dama todavía no se ha convertido en dama especial, el numero de casillas que se mueve será 1.
+        * Si la dama llega al extremo del tablero (fila 8 si es blanca o fila 1 si es negra) se convertirá
+          en dama especial modificando el atributo esDamaEspecial y poniéndolo al verdadero.
+        * Si no puede realizar dicho movimiento, debido a que la dama se sale del tablero o que no está
+          permitido porque todavía no es una dama especial, se debe lanzar una excepción del tipo
+          OperationNotSupportedException con un mensaje adecuado y no modificarán los atributos de la dama. Si
+          el movimiento es válido, se modificará la posición actual de la dama.*/
+    public void mover(){
+
+
+    }
+
+
+
+/*
+    //Probar métodos
+    @Override
+    public String toString() {
+        return "Dama{" +
+                "color=" + color +
+                ", posicion=" + posicion +
+                '}';
+    }
 
     public static void main(String[] args) {
         Dama dama = new Dama();
         System.out.println("La posición y color de la dama es:" + dama);
     }
-
-
-
-
-/*
-CODIGO 1º
-    private Posicion crearPosicionInicial() {
-        Random random = new Random(); // Crear el objeto Random
-
-        //Fila aleatoria entre 1 y 3
-        int fila = random.nextInt(3) + 1;
-        char columna = 'a';
-        //'opcion' toma valores entre 0 y 3, que representan cada caso de cada switch
-        int opcion = random.nextInt(4);
-
-        if (fila == 1 || fila == 3) {
-            switch (opcion) {
-                case 0 -> columna = 'a';
-                case 1 -> columna = 'c';
-                case 2 -> columna = 'e';
-                case 3 -> columna = 'g';
-            }
-        } else if (fila == 2) {
-            switch (opcion) {
-                case 0 -> columna = 'b';
-                case 1 -> columna = 'd';
-                case 2 -> columna = 'f';
-                case 3 -> columna = 'h';
-            }
-        }
-
-        return columna + fila;
-    }
 */
 
 
 
-    /*
-    damaBlanca = new Dama(Color.BLANCO);
-    damaNegra = new Dama(Color.NEGRO);
-    */
 
 }
