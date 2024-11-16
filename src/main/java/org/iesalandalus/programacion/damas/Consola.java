661930768c6b7104a72647f0d1ebd0aae18aadff
Desire-e <desireeruedatorresalmeria@gmail.com>
2024-11-17 00:40:18 +0100

    Creado método mostrarMenu

--- src/main/java/org/iesalandalus/programacion/damas/Consola.java
+++ src/main/java/org/iesalandalus/programacion/damas/Consola.java
@@ -5,6 +5,24 @@ public class Consola {
 
     }
 
+    /*mostrará el menú con las opciones de nuestra aplicación:
+    - crear dama por defecto,
+    - crear dama eligiendo el color,
+    - mover,
+    - salir.
+     */
+    public static void mostrarMenu(){
+        System.out.println("====================================================================");
+        System.out.println("JUEGO DE LAS DAMAS");
+        System.out.println("Elige una opción introduciendo el número correspondiente.");
+        System.out.println("====================================================================");
+        System.out.println("1. Crear dama por defecto.");
+        System.out.println("2. Crear dama eligiendo color.");
+        System.out.println("3. Mover.");
+        System.out.println("4. Salir.");
+
+    }
+
 
 
 }
