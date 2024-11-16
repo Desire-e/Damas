5cc3ec4886e1cc5c68149270ac29815f8d7ce3aa
Desire-e <desireeruedatorresalmeria@gmail.com>
2024-11-17 00:49:59 +0100

    Creado método elegirOpcionMenu

--- src/main/java/org/iesalandalus/programacion/damas/Consola.java
+++ src/main/java/org/iesalandalus/programacion/damas/Consola.java
@@ -1,27 +1,35 @@
 package org.iesalandalus.programacion.damas;
 
+import org.iesalandalus.programacion.utilidades.Entrada;
+
 public class Consola {
     private Consola(){
 
     }
 
-    /*mostrará el menú con las opciones de nuestra aplicación:
-    - crear dama por defecto,
-    - crear dama eligiendo el color,
-    - mover,
-    - salir.
-     */
+
+
+
     public static void mostrarMenu(){
         System.out.println("====================================================================");
         System.out.println("JUEGO DE LAS DAMAS");
-        System.out.println("Elige una opción introduciendo el número correspondiente.");
         System.out.println("====================================================================");
         System.out.println("1. Crear dama por defecto.");
         System.out.println("2. Crear dama eligiendo color.");
         System.out.println("3. Mover.");
         System.out.println("4. Salir.");
-
     }
+
+    public static int elegirOpcionMenu(){
+        int opcion;
+        do{
+            System.out.println("Elige una opción introduciendo el número correspondiente.");
+            opcion= Entrada.entero();
+        } while (opcion<1 || opcion>4);
+        return opcion;
+    }
+
+
 
 
 
